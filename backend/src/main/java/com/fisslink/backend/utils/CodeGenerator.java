f83153cf29b639d4afaab045a98b6dd5d4d5f833
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-04-12 20:19:29 +0100

    added all other part

--- backend/src/main/java/com/fisslink/backend/utils/CodeGenerator.java
+++ backend/src/main/java/com/fisslink/backend/utils/CodeGenerator.java
@@ -1,4 +1,16 @@
 package com.fisslink.backend.utils;
+import java.security.SecureRandom;
 
 public class CodeGenerator {
+    private static final String CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final int LENGTH = 5;
+    private static final SecureRandom random = new SecureRandom();
+
+    public static String generate() {
+        StringBuilder sb = new StringBuilder(LENGTH);
+        for (int generate = 0; generate < LENGTH; generate++) {
+            sb.append(CHARACTERS.charAt(random.nextInt(CHARACTERS.length())));
+        }
+        return sb.toString();
+    }
 }
