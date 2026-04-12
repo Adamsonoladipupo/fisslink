e998a9ff112d94669b65ae9f892cca8c709ef4dd
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-04-12 20:13:07 +0100

    added url request dto

--- backend/src/main/java/com/fisslink/backend/dtos/requests/UrlRequestDto.java
+++ backend/src/main/java/com/fisslink/backend/dtos/requests/UrlRequestDto.java
@@ -1,4 +1,12 @@
 package com.fisslink.backend.dtos.requests;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class UrlRequestDto {
+    private String originalUrl;
 }
