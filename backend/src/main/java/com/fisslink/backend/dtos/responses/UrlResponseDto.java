ac5bd42eb04bdb3421ddc1cf506da932654ba8e4
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-04-12 20:14:29 +0100

    add url response dto

--- backend/src/main/java/com/fisslink/backend/dtos/responses/UrlResponseDto.java
+++ backend/src/main/java/com/fisslink/backend/dtos/responses/UrlResponseDto.java
@@ -1,4 +1,12 @@
 package com.fisslink.backend.dtos.responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UrlResponseDto {
+    private String originalUrl;
 }
