e7168bc0ea66b2446b8ee54c42d5a80b30c170ac
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-04-12 20:26:08 +0100

    added header

---
+++ frontend/src/components/header/header.jsx
@@ -0,0 +1,20 @@
+const Header = () => {
+    return (
+        <>
+            <div>
+                <span>fisslink</span>
+                <ul>
+                    <li>Platform</li>
+                    <li>Solutions</li>
+                    <li>Resources</li>
+                    <li>Prices</li>
+                </ul>
+                <div>
+                    <button>Log in</button>
+                    <button>Start for free</button>
+                </div>
+            </div>
+        </>
+    )
+}
+export default Header
