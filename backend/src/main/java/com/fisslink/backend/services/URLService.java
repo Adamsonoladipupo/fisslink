05ce5500e0642b9fff49ad2abdc6dffec598044a
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-04-12 20:16:27 +0100

    added url service

--- backend/src/main/java/com/fisslink/backend/services/URLService.java
+++ backend/src/main/java/com/fisslink/backend/services/URLService.java
@@ -1,4 +1,40 @@
 package com.fisslink.backend.services;
 
+import com.fisslink.backend.data.models.URL;
+import com.fisslink.backend.data.repositories.URLRepository;
+import com.fisslink.backend.dtos.requests.ShortCodeRequestDto;
+import com.fisslink.backend.dtos.requests.UrlRequestDto;
+import com.fisslink.backend.dtos.responses.ShortCodeResponseDto;
+import com.fisslink.backend.dtos.responses.UrlResponseDto;
+import com.fisslink.backend.exceptions.NonExistingShortCodeException;
+import com.fisslink.backend.utils.Mappers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class URLService {
+    @Autowired
+    private URLRepository urlRepository;
+
+    public ShortCodeResponseDto shortUrl(UrlRequestDto requestDto){
+        if (urlRepository.existsByOriginalUrl(requestDto.getOriginalUrl())){
+            URL oldUrl = urlRepository.findByOriginalUrl(requestDto.getOriginalUrl());
+            return Mappers.shortCodeResponse(oldUrl);
+        }
+
+        URL newUrl = Mappers.urlRequest(requestDto);
+        if(urlRepository.existsByShortCode(newUrl.getShortCode())){
+            newUrl = Mappers.urlRequest(requestDto);
+        }
+        urlRepository.save(newUrl);
+        return Mappers.shortCodeResponse(newUrl);
+    }
+
+    public UrlResponseDto getOriginalUrl(ShortCodeRequestDto requestDto){
+        if(urlRepository.existsByShortCode(requestDto.getShortCode())){
+            URL oldUrl = urlRepository.findByShortCode(requestDto.getShortCode());
+            return Mappers.urlResponse(oldUrl);
+        }
+        else throw new NonExistingShortCodeException("Sorry, this short code does not exist");
+    }
 }
