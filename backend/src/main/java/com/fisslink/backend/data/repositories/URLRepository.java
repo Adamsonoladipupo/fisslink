e74f7d593dd3ad56e68b7af2930a722427732613
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-04-12 20:11:55 +0100

    added url repository

--- backend/src/main/java/com/fisslink/backend/data/repositories/URLRepository.java
+++ backend/src/main/java/com/fisslink/backend/data/repositories/URLRepository.java
@@ -1,4 +1,13 @@
 package com.fisslink.backend.data.repositories;
 
-public interface URLRepository {
+import com.fisslink.backend.data.models.URL;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface URLRepository extends JpaRepository<URL, Long> {
+    URL findByShortCode(String shortCode);
+    URL findByOriginalUrl(String originalUrl);
+    boolean existsByOriginalUrl(String originalUrl);
+    boolean existsByShortCode(String shortCode);
 }
