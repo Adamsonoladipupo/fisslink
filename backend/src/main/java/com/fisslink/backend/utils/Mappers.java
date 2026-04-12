1cac460b79d9ef6c5e39b2754256c4cba420bc6f
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-04-12 20:17:52 +0100

    added mapper class

--- backend/src/main/java/com/fisslink/backend/utils/Mappers.java
+++ backend/src/main/java/com/fisslink/backend/utils/Mappers.java
@@ -1,4 +1,30 @@
 package com.fisslink.backend.utils;
 
+import com.fisslink.backend.data.models.URL;
+import com.fisslink.backend.dtos.requests.UrlRequestDto;
+import com.fisslink.backend.dtos.responses.ShortCodeResponseDto;
+import com.fisslink.backend.dtos.responses.UrlResponseDto;
+
+import java.time.LocalDateTime;
+
 public class Mappers {
+    public static URL urlRequest(UrlRequestDto requestDto){
+        URL url = new URL();
+        url.setOriginalUrl(requestDto.getOriginalUrl());
+        url.setShortCode(CodeGenerator.generate());
+        url.setCreatedAt(LocalDateTime.now());
+        return url;
+    }
+
+    public static ShortCodeResponseDto shortCodeResponse(URL url ){
+        ShortCodeResponseDto responseDto =  new ShortCodeResponseDto();
+        responseDto.setShortCode(url.getShortCode());
+        return responseDto;
+    }
+
+    public static UrlResponseDto urlResponse(URL url){
+        UrlResponseDto responseDto = new UrlResponseDto();
+        responseDto.setOriginalUrl(url.getOriginalUrl());
+        return responseDto;
+    }
 }
