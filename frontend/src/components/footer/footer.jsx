571604b3b63aa1775f98b97b18d45785602df060
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-04-12 20:25:11 +0100

    added footer jsx file

---
+++ frontend/src/components/footer/footer.jsx
@@ -0,0 +1,14 @@
+const Footer = () =>{
+    return(
+        <>
+            <div>
+                <span>
+                    fisslink @ Copyright 2025
+                </span>
+                <span>Terms of use</span>
+                <span>Social icons</span>
+            </div>
+        </>
+    )
+}
+export default Footer
